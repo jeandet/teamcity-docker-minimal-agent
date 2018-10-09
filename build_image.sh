d96b9f12f1e09b161e6a4d1d856e23bca0e5c3a2
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2018-10-09 23:21:23 +0200

    Auto update base agent and added Docker into it
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- build_image.sh
+++ build_image.sh
@@ -1,1 +1,6 @@
 docker build -t jeandet/teamcity-docker-minimal-agent .
+docker run -d=true -e SERVER_URL=https://hephaistos.lpp.polytechnique.fr/teamcity --name=teamcity-docker-minimal-agent -it jeandet/teamcity-docker-minimal-agent &
+sleep 300
+docker stop teamcity-docker-minimal-agent
+docker commit teamcity-docker-minimal-agent jeandet/teamcity-docker-minimal-agent
+docker rm teamcity-docker-minimal-agent
